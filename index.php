6b5287dccbdd5a3cf7931c7e5feb56a23babb140
efarinholt <evan@fireinspace.net>
2014-10-29 19:44:51 -0400

    Basic HTML template using PHP

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<head>
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+<meta name="robots" content="<?php echo $meta_index; ?>" />
+<meta name="description" content="<?php echo $meta_description; ?>" />
+<meta name="keywords" content="<?php echo $meta_keywords; ?>" />
+<meta name="author" content="<?php echo $meta_author; ?>" />
+
+
+<script type="text/javascript" src="<?php echo $metaroot; ?>js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="<?php echo $metaroot; ?>js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo $metaroot; ?>js/bootstrapValidator.js"></script>
+
+<link rel="stylesheet" href="<?php echo $metaroot; ?>css/bootstrap.min.css" />
+<link rel="stylesheet" href="<?php echo $metaroot; ?>css/main.css" />
+
+<!--[if lt IE 9]>
+	<script src="<?php echo $metaroot; ?>js/html5shiv.js"></script>
+	<script src="<?php echo $metaroot; ?>js/respond.min.js"></script>
+<![endif]-->
+
+<title></title>
+
+</head>
+<body>
+</body>
+</html>
